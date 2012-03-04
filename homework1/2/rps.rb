fec00d4d5cfc8242b0a434bb106a999298e33999
Roman Tymoshuk <rtym@softserveinc.com>
2012-03-05 00:09:50 +0200

    2b task implemented

--- homework1/2/rps.rb
+++ homework1/2/rps.rb
@@ -3,6 +3,7 @@ class NoSuchStrategyError < StandardError ; end
 
 def rps_game_winner(game = [])
   raise WrongNumberOfPlayersError unless game.length == 2
+  raise NoSuchStrategyError unless is_game?game
 
   first_strategy = game[0][1].downcase
   second_strategy = game[1][1].downcase
@@ -20,4 +21,22 @@ def rps_game_winner(game = [])
   else
     raise NoSuchStrategyError
   end
+end
+
+def is_strategy?(strategy = [])
+  strategy.is_a?(Array) && strategy.length == 2 && strategy[0].is_a?(String) && strategy[1].is_a?(String) && strategy[1].length == 1 && (strategy[1].downcase == 'r' || strategy[1].downcase == 's' || strategy[1].downcase == 'p')
+end
+
+def is_game?(game = [])
+  game.is_a?(Array) && game.length == 2 && is_strategy?(game[0]) && is_strategy?(game[1]) 
+end
+
+def rps_tournament_winner(game = [])
+  unless is_game?game
+    game.each_index do |key|
+      game[key] = rps_tournament_winner game[key]
+    end
+  end
+  
+  return rps_game_winner game
 end
--- homework1/2/rps_spec.rb
+++ homework1/2/rps_spec.rb
@@ -3,7 +3,7 @@
 
 require './rps.rb'
 
-describe "#Simple game" do
+describe "rps_game_winner" do
   it "Same strategies #1" do
     strategies = [ ['Petia', 'S'], ['Vasia', 'S'] ]
     rps_game_winner(strategies).should == strategies[0]
@@ -55,5 +55,60 @@ describe "#Simple game" do
 
   it "Should raise NoSuchStrategyError" do
     lambda { rps_game_winner([ ["Petia", "Z"], ["Vasia", "K"] ]) }.should raise_error(NoSuchStrategyError)
+  end
+end
+
+describe "#is_strategy?" do
+  it "Simple strategy" do
+    is_strategy?(["Armando", "P"]).should == true
+  end
+
+  it "Two strategies" do
+    is_strategy?([ ["Armando", "P"], ["Belmondo", "S"] ]).should == false
+  end
+
+  it "Empty test" do
+    is_strategy?.should == false
+  end
+
+  it "Incorrect strategy letter" do
+    is_strategy?(["Belmondo", "Z"]).should == false
+  end
+end
+
+describe "#is_game?" do
+  it "Simple game" do
+    is_game?([ ["Armando", "P"], ["Belmondo", "S"] ]).should == true
+  end
+
+  it "Empty game" do
+    is_game?.should == false
+  end
+
+  it "Check simple strategy" do
+    is_game?(["Armando", "P"]).should == false
+  end
+end
+
+describe "rps_tournament_winner" do
+  it "Book example" do
+    rps_tournament_winner([
+        [
+          [
+            ["Armando", "P"], ["Dave", "S"]
+          ],
+          [ 
+            ["Richard", "R"], ["Michael", "S"]
+          ],
+        ],
+        [
+          [ 
+            ["Allen", "S"], ["Omer", "P"]
+          ],
+          [
+            ["David E.", "R"], ["Richard X.", "P"]
+          ]
+        ]
+      ]).should == ["Richard", "R"]
   end
 end
